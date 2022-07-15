69a9c40f483c36bc35ebd31999942b808539667e
Jonathan <jonathanmoorevla@gmail.com>
2022-07-14 19:08:23 -0500

    quick fix, re-add dog pic functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 // console.log('%c HI', 'color: firebrick')
 
 
-// function renderDog(dogPic) {
-//     let card = document.createElement('p')
-//     card.className = 'dogPics'
-//     card.innerHTML = `<img src='${dogPic}'>`
-//     document.querySelector('#dog-image-container').appendChild(card);
-// }
-// // Fetch Requests
+function renderDog(dogPic) {
+    let card = document.createElement('p')
+    card.className = 'dogPics'
+    card.innerHTML = `<img src='${dogPic}'>`
+    document.querySelector('#dog-image-container').appendChild(card);
+}
+// Fetch Requests
 
-// //gets dogs photos
-// window.addEventListener('DOMContentLoaded', () => {
-//     fetch('https://dog.ceo/api/breeds/image/random/4')
-//     .then(res => res.json())
-//     .then(dogData => dogData.message.forEach(dogPic => renderDog(dogPic))) 
-// })
+//gets dogs photos
+window.addEventListener('DOMContentLoaded', () => {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(res => res.json())
+    .then(dogData => dogData.message.forEach(dogPic => renderDog(dogPic))) 
+})
 
 //gets dogs list
 window.addEventListener('DOMContentLoaded', () => {
